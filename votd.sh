153f0bd8eac8c28c108a0a6118b3db4b431e441e
Ava Gailliot <mztriz@gmail.com>
2016-07-13 11:22:36 -0400

    Update votd.sh
    removed unnecessary sed command

--- votd.sh
+++ votd.sh
@@ -7,7 +7,7 @@ URL=https://www.biblegateway.com/usage/votd/rss/votd.rdf?51
 
 # Grab VOTD from XML in URL
 MESSAGE=$(curl --silent --connect-timeout 10 "$URL" | grep -o '&ldquo.*' | cut -f2- -d';' | cut -d '&' -f1 )
-VERSE=$(curl --silent --connect-timeout 10 "$URL" | sed -n '22 p'| sed -e 's/<\/title>//g' -e 's/<[^>]*>//g')
+VERSE=$(curl --silent --connect-timeout 10 "$URL" | sed -n '22 p'| sed -e 's/<[^>]*>//g')
 
 # Print Verse of the Day
 echo "$MESSAGE $VERSE"
