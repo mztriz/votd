a97816afdebd4e333a068f8e53e3381b539f598b
Ava Gailliot <mztriz@gmail.com>
2016-09-12 16:20:45 -0400

    Added newline and tab for readability

--- votd.sh
+++ votd.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 # Ava Gailliot
-# A small script to print Bible Gateway's  VOTD in NLT
+# A small shell script to print Bible Gateway's VOTD in NLT
 
 # RSS feed URL for BG
+# 51 is NLT
 URL=https://www.biblegateway.com/usage/votd/rss/votd.rdf?51
 
 # Grab VOTD from XML in URL
 MESSAGE=$(curl --silent --connect-timeout 10 "$URL" | grep -o '&ldquo.*' | cut -f2- -d';' | cut -d '&' -f1 )
 VERSE=$(curl --silent --connect-timeout 10 "$URL" | sed -n '22 p'| sed -e 's/<[^>]*>//g')
-
-# Print Verse of the Day
-echo "$MESSAGE $VERSE"
+echo -e "\"$MESSAGE\" \n\t\t$VERSE"
